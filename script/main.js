8a03d2f0d66b2dc40d6bde16484ce14131928f09
Artem <67517973+KingArthur-IT@users.noreply.github.com>
2022-10-29 14:42:25 +0300

    clients edits

--- script/main.js
+++ script/main.js
@@ -1,48 +1,9 @@
-const clientCarouselClass = 'our-clients__list';
+const clientCarouselClass = 'our-clients__carousel';
 const resumeCarouselClass = 'resume__form-wrapper';
 
 $(document).ready(function(){
-  carouselSetup();
   startFormCarousel();
 });
-
-$(window).resize(function() {
-  carouselSetup();
-});
-
-function carouselSetup(){
-  if ( $(window).width() < 1024 ) {
-    startClientsCarousel();
-    $(`.${clientCarouselClass}`).removeClass('off');
-  } else {
-    $(`.${clientCarouselClass}`).addClass('off');
-    $(`.${clientCarouselClass}`).removeClass('owl-carousel');
-  }
-}
-
-function startClientsCarousel(){
-  $(`.${clientCarouselClass}`).addClass('owl-carousel')
-  $(`.${clientCarouselClass}`).owlCarousel({
-    loop: true,
-    slideTransition: 'linear',
-    autoplayTimeout: 2000,
-    autoplaySpeed: 2000,
-    nav: false,
-    dots: false,
-    center: true,
-    responsive:{
-      0:{
-        items: 2.2
-      },
-      425:{
-        items: 2.2
-      },
-      768:{
-        items: 3.3,
-      },
-    }
-  });
-};
 function startFormCarousel(){
   const resumeCarousel = $(`.${resumeCarouselClass}`).owlCarousel({
     loop: false,
@@ -64,12 +25,6 @@ function startFormCarousel(){
     resumeCarousel.trigger('prev.owl.carousel');
   });
 };
-
-function stopClientsCarousel(className) {
-    const owl = $(`.${className}`);
-    owl.trigger('destroy.owl.carousel');
-    owl.addClass('off');
-}
 
 
 //menu
