4f00e898a5868c3530ac0ec5ee575a57fe3d81ef
Artem <67517973+KingArthur-IT@users.noreply.github.com>
2022-11-21 21:49:05 +0300

    calc modal & edits

--- script/modal.js
+++ script/modal.js
@@ -1,4 +1,5 @@
 const contactsModal = document.querySelector('.contacts');
+const costModal = document.querySelector('.cost');
 
 if (contactsModal){
     //open contacts modal
@@ -22,4 +23,28 @@ if (contactsModal){
     document.querySelector('.contacts__hero .modal__close').addEventListener('click', () => {closeContactsModal()});
     contactsModal.addEventListener('click', () => {closeContactsModal()});
     document.querySelector('.contacts__hero').addEventListener('click', (e) => {e.stopPropagation()});
+};
+
+if (costModal){
+    //open contacts modal
+    document.querySelectorAll('.open-cost-modal').forEach((el) => {
+        el.addEventListener('click', () => {
+            costModal.classList.add('modal-dispalayed');
+            setTimeout(() => {
+                costModal.classList.add('modal-visible');
+            }, 200);
+        })
+    });
+    
+    //close cost modal
+    const closeCostModal = () => {
+        costModal.classList.remove('modal-visible');
+        setTimeout(() => {
+            costModal.classList.remove('modal-dispalayed');
+        }, 300);
+    };
+    
+    document.querySelector('.cost__hero .modal__close').addEventListener('click', () => {closeCostModal()});
+    costModal.addEventListener('click', () => {closeCostModal()});
+    document.querySelector('.cost__hero').addEventListener('click', (e) => {e.stopPropagation()});
 }
