3e6af043e612f37639ee7688b1db7dcec3e255c9
Artem <67517973+KingArthur-IT@users.noreply.github.com>
2023-01-05 19:50:37 +0300

    edit fetch

--- script/catalog.js
+++ script/catalog.js
@@ -14,19 +14,21 @@
 //        })
 //    })
 
-fetch('../csv/csvjson.json')
-   .then(response => response.json())
-   .then(data => {
-      data.forEach(e => {
-         let vals = Object.values(e);
-         let row = `
-            <td>${vals[2]}</td>
-            <td>${vals[3]}</td>
-            <td>${vals[1]}</td>
-            <td>${vals[7]}</td>
-         `
-         let ce = document.createElement('tr');
-         ce.innerHTML = row;
-         document.querySelector('table').appendChild(ce);
-      });
-   })
+document.addEventListener('DOMContentLoaded', () => {
+   fetch('https://kingarthur-it.github.io/stretchCeiling/csv/csvjson.json')
+      .then(response => response.json())
+      .then(data => {
+         data.forEach(e => {
+            let vals = Object.values(e);
+            let row = `
+               <td>${vals[2]}</td>
+               <td>${vals[3]}</td>
+               <td>${vals[1]}</td>
+               <td>${vals[7]}</td>
+            `
+            let ce = document.createElement('tr');
+            ce.innerHTML = row;
+            document.querySelector('table').appendChild(ce);
+         });
+      })
+})
